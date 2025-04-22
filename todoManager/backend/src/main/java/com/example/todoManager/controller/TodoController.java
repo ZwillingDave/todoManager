a7fa4e98b354b81a7694dfb2c469085e35357d2e
David Windisch <zwilling_dave@outlook.com>
2025-04-22 08:20:30 +0200

    added TodoController with findById

---
+++ todoManager/backend/src/main/java/com/example/todoManager/controller/TodoController.java
@@ -0,0 +1,32 @@
+package com.example.todoManager.controller;
+
+import com.example.todoManager.model.Todo;
+import com.example.todoManager.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable String todoId) {
+        try {
+            Todo todo = todoService.findById(todoId);
+            return ResponseEntity.ok(todo);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
