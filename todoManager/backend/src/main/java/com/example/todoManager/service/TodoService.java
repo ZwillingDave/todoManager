25080192f62062a2cc899a88ab5b82cc6272e3fa
David Windisch <zwilling_dave@outlook.com>
2025-04-22 08:10:01 +0200

    added TodoService with save and findById Method

---
+++ todoManager/backend/src/main/java/com/example/todoManager/service/TodoService.java
@@ -0,0 +1,32 @@
+package com.example.todoManager.service;
+
+import com.example.todoManager.model.Todo;
+import com.example.todoManager.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TodoService {
+//    private final TodoOwnerResolverRegistry resolverRegistry;
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+//        this.resolverRegistry = registry;
+        this.todoRepository = todoRepository;
+    }
+
+    public void save(Todo todo) {
+        todoRepository.save(todo);
+    }
+
+    public Todo findById(String todoId) {
+        Optional<Todo> todo = todoRepository.findById(todoId);
+        if (todo.isEmpty()) {
+            throw new RuntimeException("Todo not found");
+        }
+        return todo.get();
+    }
+}
