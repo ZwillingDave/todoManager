b2b9406ade2d69a10e8a391d509378cf84f0fff6
David Windisch <zwilling_dave@outlook.com>
2025-04-29 10:59:50 +0200

    added TokenService and Tests

---
+++ todoManager/backend/src/main/java/com/example/todoManager/service/TokenService.java
@@ -0,0 +1,60 @@
+package com.example.todoManager.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.todoManager.exception.JWTValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+    private JWTWrapper jwtWrapper;
+
+    @Autowired
+    public TokenService(JWTWrapper jwtWrapper) {
+        this.jwtWrapper = jwtWrapper;
+    }
+
+    public String createToken(String uuid, String userName, Instant expiryDate) {
+        String jwt = "";
+        JWTValidationException exception = new JWTValidationException();
+        if (userName == null || userName.isEmpty()) {
+            exception.addMessage("Username not valid when creating new JWT.");
+        }
+        if (expiryDate == null) {
+            exception.addMessage("Expiry date not valid when creating new JWT.");
+        }
+        try {
+
+            jwt = jwtWrapper.createJWTToken(uuid ,userName, expiryDate);
+
+        } catch (JWTCreationException jwtCreationException) {
+            exception.addMessage(jwtCreationException.getMessage());
+        }
+        if (exception.shouldBeThrown()) {
+            throw exception;
+        }
+        return jwt;
+    }
+
+    public void validateToken(String token) throws JWTVerificationException {
+        Algorithm algorithm = Algorithm.HMAC256("Kaiser");
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        verifier.verify(token);
+    }
+
+    public String getUserNameFromToken(String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        if (jwt.getClaim("username").isNull()) {
+            throw new JWTDecodeException("Invalid Token");
+        }
+        return jwt.getClaim("username").asString();
+    }
+}
---
+++ todoManager/backend/src/test/java/com/example/todoManager/service/TokenServiceTest.java
@@ -0,0 +1,111 @@
+package com.example.todoManager.service;
+
+
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.todoManager.exception.JWTValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TokenServiceTest {
+
+    TokenService tokenService;
+
+    @Mock
+    JWTWrapper jwtWrapper;
+    String uuid = "uuid";
+    String email = "user@email.at";
+    Instant expiryDate = Instant.parse("2025-03-14T08:56:23.793842400Z");
+    String validToken = "token";
+
+
+    @BeforeEach
+    void setUp() {
+        tokenService = new TokenService(jwtWrapper);
+    }
+
+    @Test
+    void createToken_valid() {
+        Mockito.when(jwtWrapper.createJWTToken(uuid, email, expiryDate)).thenReturn(validToken);
+        String result = tokenService.createToken(uuid, email, expiryDate);
+        assertEquals(validToken, result);
+    }
+
+    @Test
+    void createToken_invalidParameters() {
+        List<String> messages = List.of("Expiry date not valid when creating new JWT.", "Username not valid when creating new JWT.");
+        JWTValidationException exception = assertThrows(JWTValidationException.class, () -> tokenService.createToken(null, null, null));
+        assertTrue(exception.getMessages().containsAll(messages));
+
+        messages = List.of("Username not valid when creating new JWT.");
+        JWTValidationException usernameNullException = assertThrows(JWTValidationException.class, () -> tokenService.createToken(null, null, expiryDate));
+        assertTrue(usernameNullException.getMessages().containsAll(messages));
+
+        messages = List.of("Expiry date not valid when creating new JWT.");
+        JWTValidationException expiryDateNullException = assertThrows(JWTValidationException.class, () -> tokenService.createToken(uuid, email, null));
+        assertTrue(expiryDateNullException.getMessages().containsAll(messages));
+    }
+
+    @Test
+    void createToken_shouldThrowValidationException() {
+        Mockito.when(jwtWrapper.createJWTToken(uuid, email, expiryDate)).thenThrow(new JWTCreationException("Bla", null));
+        JWTValidationException jwtValidationException = assertThrows(JWTValidationException.class, () -> {
+            tokenService.createToken(uuid, email, expiryDate);
+        });
+        assertTrue(jwtValidationException.containsMessage("Bla"));
+    }
+
+    @Test
+    void validateTokenDoesNotThrowException() {
+        String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjU3NDE5NDI1ODN9.0xkhKS8VKl-m4meKIEkd6-qXm_OZox1EkvWzNUTphLs";
+        tokenService.validateToken(validToken);
+    }
+
+    @Test
+    void validateTokenThrowsExceptionWithInvalidToken() {
+        assertThrows(JWTVerificationException.class, () -> {
+            tokenService.validateToken("test");
+        });
+    }
+
+    @Test
+    void validateToken_failsWithExpiredDate() {
+        String invalidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjE3NDE5NDI1ODN9.q7OZfffwwH_U0k2j6oI1__mTgOw2R6JxNzYTllfQK_Q";
+        assertThrows(JWTVerificationException.class, () -> {
+            tokenService.validateToken(invalidToken);
+        });
+    }
+
+
+    @Test
+    void validateToken_failsWithIncorrectSignature() {
+        String invalidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjU3NDE5NDI1ODN9.qoXXnVlkQh4suvSL1EE-C1d7tzhxfUCc5obOn3uj-bE";
+        assertThrows(JWTVerificationException.class, () -> {
+            tokenService.validateToken(invalidToken);
+        });
+    }
+
+    @Test
+    void getUserNameFromToken_returnsUsername() {
+        String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjU3NDE5NDI1ODN9.0xkhKS8VKl-m4meKIEkd6-qXm_OZox1EkvWzNUTphLs";
+        String userNameFromToken = tokenService.getUserNameFromToken(validToken);
+        assertEquals("user", userNameFromToken);
+    }
+
+    @Test
+    void getUserNameFromToken_throwsException() {
+        String tokenWithNullUsername = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6bnVsbCwiZXhwIjoxNzQzNzY0MDQ0fQ.wqMX61nYap_RuBlz-L8lK6QkHUuc4oIoIqqpwpJ20RE";
+        assertThrows(JWTDecodeException.class, () -> tokenService.getUserNameFromToken(tokenWithNullUsername));
+    }
+}
