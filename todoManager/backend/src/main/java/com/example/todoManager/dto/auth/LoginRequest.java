921aa3b83cbf7bc94cc1e820e71ff8e598897865
David Windisch <zwilling_dave@outlook.com>
2025-04-29 11:00:43 +0200

    added some lombok annotations

--- todoManager/backend/src/main/java/com/example/todoManager/dto/auth/LoginRequest.java
+++ todoManager/backend/src/main/java/com/example/todoManager/dto/auth/LoginRequest.java
@@ -1,5 +1,12 @@
 package com.example.todoManager.dto.auth;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
     private String email;
     private String password;
