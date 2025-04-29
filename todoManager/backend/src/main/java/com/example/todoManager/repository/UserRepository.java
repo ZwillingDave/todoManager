e33118fe1f293e563f628ee5ec4d1c7de06f4341
David Windisch <zwilling_dave@outlook.com>
2025-04-29 10:59:07 +0200

    added findByEmail to repo

--- todoManager/backend/src/main/java/com/example/todoManager/repository/UserRepository.java
+++ todoManager/backend/src/main/java/com/example/todoManager/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.todoManager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> , JpaSpecificationExecutor<User> {
 
+    Optional<User> findByEmail(String email);
 }
