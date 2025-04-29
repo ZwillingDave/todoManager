f06f6caa3b20ba2f18e57bc31b6cd31557f2c4f8
David Windisch <zwilling_dave@outlook.com>
2025-04-29 11:00:58 +0200

    added JWTValidationException

---
+++ todoManager/backend/src/main/java/com/example/todoManager/exception/JWTValidationException.java
@@ -0,0 +1,28 @@
+package com.example.todoManager.exception;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JWTValidationException extends RuntimeException {
+    private List<String> messages = new ArrayList<>();
+
+    public JWTValidationException() {
+    }
+
+    public void addMessage(String message) {
+        messages.add(message);
+    }
+
+    public boolean containsMessage(String message) {
+        return messages.contains(message);
+    }
+
+    public boolean shouldBeThrown() {
+        return !messages.isEmpty();
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+}
