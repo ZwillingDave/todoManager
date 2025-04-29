1e2f774dacc22616e032809454817c4d399a0b50
David Windisch <zwilling_dave@outlook.com>
2025-04-29 12:34:10 +0200

    added /api to endpoint mappings

--- todoManager/backend/src/main/java/com/example/todoManager/controller/GroupController.java
+++ todoManager/backend/src/main/java/com/example/todoManager/controller/GroupController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("groups")
+@RequestMapping("/api/groups")
 public class GroupController {
     private final GroupService groupService;
 
--- todoManager/backend/src/main/java/com/example/todoManager/controller/TodoController.java
+++ todoManager/backend/src/main/java/com/example/todoManager/controller/TodoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/api/todos")
 public class TodoController {
     private final TodoService todoService;
 
