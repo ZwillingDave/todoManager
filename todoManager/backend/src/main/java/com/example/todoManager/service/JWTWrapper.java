521a20912a734245ce87e883bc46f5f6291b1f6d
David Windisch <zwilling_dave@outlook.com>
2025-05-13 15:09:51 +0200

    added secret variable to JWTWrapper and changed secret for the algorithm

--- todoManager/backend/src/main/java/com/example/todoManager/service/JWTWrapper.java
+++ todoManager/backend/src/main/java/com/example/todoManager/service/JWTWrapper.java
@@ -3,14 +3,19 @@ package com.example.todoManager.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 // TODO: change secret
 @Service
 public class JWTWrapper {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
     public String createJWTToken(String uuid, String email, Instant expiryDate) throws JWTCreationException {
-        Algorithm algorithm = Algorithm.HMAC256("Kaiser");
+        Algorithm algorithm = Algorithm.HMAC256(secret);
         return JWT.create()
                 .withClaim("uuid", uuid)
                 .withClaim("email", email)
