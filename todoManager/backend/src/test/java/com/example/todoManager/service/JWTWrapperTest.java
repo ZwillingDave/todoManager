a40a96fda5c3368a34b4ac6915934bf2635d2a2b
David Windisch <zwilling_dave@outlook.com>
2025-05-20 08:11:57 +0200

    Update JWTWrapper tests to use ReflectionTestUtils for secrets

--- todoManager/backend/src/test/java/com/example/todoManager/service/JWTWrapperTest.java
+++ todoManager/backend/src/test/java/com/example/todoManager/service/JWTWrapperTest.java
@@ -1,7 +1,7 @@
 package com.example.todoManager.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.Instant;
 
@@ -12,22 +12,26 @@ class JWTWrapperTest {
     String username = "user";
     String uuid = "uuid";
     Instant expiryDate = Instant.parse("2025-03-14T08:56:23.793842400Z");
-    String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoidXVpZCIsImVtYWlsIjoidXNlciIsImV4cCI6MTc0MTk0MjU4M30.vJzjtv1yITx1SKp6pcY_e0R6FPHOx4UVvkqpD2Niwkc";
+    String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoidXVpZCIsInVzZXJuYW1lIjoidXNlciIsImV4cCI6MTc0MTk0MjU4M30.qkm9yGlHoaJ-82ZrY7mhP8-sKt997CG8dMODGqd2hns";
+
 
     @Test
     void createJWTToken_valid() {
+        ReflectionTestUtils.setField(jwtWrapper, "secret", "my-test-secret-for-java-unittest");
         String token = jwtWrapper.createJWTToken(uuid, username, expiryDate);
         assertEquals(validToken, token);
     }
 
     @Test
     void createJWTToken_invalid(){
+        ReflectionTestUtils.setField(jwtWrapper, "secret", "my-test-secret-for-java-unittest");
         String token = jwtWrapper.createJWTToken(null, null, null);
         assertNotEquals(validToken, token);
     }
 
     @Test
     void createJWTToken_differentDate() {
+        ReflectionTestUtils.setField(jwtWrapper, "secret", "my-test-secret-for-java-unittest");
         String token = jwtWrapper.createJWTToken(uuid, username, Instant.now());
         assertNotEquals(validToken, token);
     }
