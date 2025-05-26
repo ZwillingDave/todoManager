7e53ff949dec0f7e9c9aa216b4c766d74bfc46e5
David Windisch <zwilling_dave@outlook.com>
2025-05-26 12:14:38 +0200

    Refactor CSS file structure and enhance auth logic.
    Moved `App.css` to a dedicated `styles` directory for better organization. Updated login logic to include token expiry handling and streamlined signup to auto-fill login fields upon success. Simplified and formatted code for improved readability.

--- todoManager/frontend/src/App.tsx
+++ todoManager/frontend/src/App.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import * as http from "node:http";
+import {useNavigate} from 'react-router-dom';
+import './styles/App.css';
 
 
 function App() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const login = async () => {
 
         const email = document.getElementById("login-email") as HTMLInputElement;
         const password = document.getElementById("login-password") as HTMLInputElement;
         await window.fetch("http://localhost:8080/api/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    email: email.value,
-                    password: password.value
-                }),
-            }).then(response => response.ok ? response.json() : alert("Invalid credentials"))
-                .then(data => {
-                    if(data.success){
-                        Cookies.set("token", data.token);
-                        navigate(data.path);
-                    }
-                })
-                .catch(error => alert(error))
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            }),
+        }).then(response => response.ok ? response.json() : alert("Invalid credentials"))
+            .then(async data => {
+                if (data && data.success) {
+                    Cookies.set("token", data.token);
+
+
+                    const res = await fetch("http://localhost:8080/api/auth/decodePayload", {
+                        method: "GET",
+                        credentials: "include",
+                    });
+
+                    const payload = await res.json();
+                    const expiryDate = new Date(payload.expiryDate);
+
+                    Cookies.set("token", data.token, {expires: expiryDate});
+                    navigate(data.path);
+                }
+            })
+            .catch(error => alert(error))
         ;
 
     }
@@ -44,7 +53,7 @@ function App() {
             alert("Passwords do not match");
             return;
         }
-        const response = await window.fetch("http://localhost:8080/api/auth/signup", {
+        await window.fetch("http://localhost:8080/api/auth/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -54,38 +63,51 @@ function App() {
                 email: email.value,
                 password: password.value
             }),
-        })
-        const data = await response.json();
-        console.log(data.message);
+        }).then(response => response.ok ? response.json() : alert("Something went wrong. Try again."))
+            .then(() => {
+                    const loginMail = document.getElementById("login-email") as HTMLInputElement;
+                    const loginpw = document.getElementById("login-password") as HTMLInputElement;
+                    loginMail.value = email.value;
+                    loginpw.value = password.value;
+                    login();
+
+                }
+            );
     }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <section>
-          <h1>TodoManager</h1>
-        </section>
-        <section className={"auth-section"}>
-          <form className={"login-field"} onSubmit={event => {event.preventDefault(); login()}}>
-            <h1>Login</h1>
-            <input id={"login-email"} type={"email"} placeholder={"Email"}></input>
-            <input id={"login-password"} type={"password"} placeholder={"Password"}></input>
-            <button id={"login-btn"}>Login</button>
-          </form>
-          <form className={"signup-field"} onSubmit={event => {event.preventDefault(); signup()}}>
-            <h1>Signup</h1>
-            <input id={"signup-username"} type={"text"} placeholder={"Username"}></input>
-            <input id={"signup-email"} type={"email"} placeholder={"Email"}></input>
-            <input id={"signup-password"} type={"password"} placeholder={"Password"}></input>
-            <input id={"signup-confirmPassword"} type={"password"} placeholder={"Confirm Password"}></input>
-            <button id={"signup-btn"}>Signup</button>
-          </form>
-        </section>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <section>
+                    <h1>TodoManager</h1>
+                </section>
+                <section className={"auth-section"}>
+                    <form className={"login-field"} onSubmit={event => {
+                        event.preventDefault();
+                        login()
+                    }}>
+                        <h1>Login</h1>
+                        <input id={"login-email"} type={"email"} placeholder={"Email"}></input>
+                        <input id={"login-password"} type={"password"} placeholder={"Password"}></input>
+                        <button id={"login-btn"}>Login</button>
+                    </form>
+                    <form className={"signup-field"} onSubmit={event => {
+                        event.preventDefault();
+                        signup()
+                    }}>
+                        <h1>Signup</h1>
+                        <input id={"signup-username"} type={"text"} placeholder={"Username"}></input>
+                        <input id={"signup-email"} type={"email"} placeholder={"Email"}></input>
+                        <input id={"signup-password"} type={"password"} placeholder={"Password"}></input>
+                        <input id={"signup-confirmPassword"} type={"password"} placeholder={"Confirm Password"}></input>
+                        <button id={"signup-btn"}>Signup</button>
+                    </form>
+                </section>
 
 
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
